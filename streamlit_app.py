4f786543629b779f2d9877dcab2b39ae1c5c47e9
Abubakar Sani <34723057+abubakars@users.noreply.github.com>
2025-07-31 18:35:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ credentials = {
 }
 
 # 🔗 Replace this with YOUR sheet export link
-sheet_url = "https://docs.google.com/spreadsheets/d/1gja9oO9lhLlC8DQshOjPIkRbhFU2nrCE9Kuljs7-6vk/edit?gid=697693047#gid=697693047"
+sheet_url = "https://docs.google.com/spreadsheets/d/1gja9oO9lhLlC8DQshOjPIkRbhFU2nrCE9Kuljs7-6vk/edit?gid=697693047#gid=697693047.csv"
 
 @st.cache_data
 def load_data_from_gsheet(url):
