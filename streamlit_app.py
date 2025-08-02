7dd9da275ff3eae5fd53a70fd3dd3e1bfb519cdf
Abubakar Sani <34723057+abubakars@users.noreply.github.com>
2025-08-02 17:36:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-
-st.markdown("# Niger MLoS")
 st.markdown(
-    "<h1 style='text-align: center; color: #2c3e50;'>Niger MLoS</h1>",
+    "<h1 style='text-align: center; color: #0000FF;'>Niger MLoS</h1>",
     unsafe_allow_html=True
 )
 
