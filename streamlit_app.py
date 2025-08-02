170322bbc823912c37cf5dba9dc7ae82b9830468
Abubakar Sani <34723057+abubakars@users.noreply.github.com>
2025-08-02 18:25:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from io import StringIO
 
 st.markdown("<h1 style='text-align: center;'>Niger MLoS</h1>", unsafe_allow_html=True)
 
-raw_url = "https://github.com/abubakars/mlos-niger-update/blob/main/MLOSS.csv"
+raw_url = "https://raw.githubusercontent.com/abubakars/mlos-niger-update/refs/heads/main/MLOSS.csv"
 
 resp = requests.get(raw_url)
 if resp.status_code == 200:
