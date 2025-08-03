b8df44c637d78799926b3b679889c56b65724baf
Abubakar Sani <34723057+abubakars@users.noreply.github.com>
2025-08-03 13:22:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Niger MLoS", layout="wide")
 st.markdown("<h1 style='text-align: center;'>Niger MLoS</h1>", unsafe_allow_html=True)
 
 # --- Load data from GitHub ---
-raw_url = "https://raw.githubusercontent.com/abubakars/mlos-niger-update/refs/heads/main/MLOSS.csv"
+raw_url = "https://raw.githubusercontent.com/abubakars/mlos-niger-update/refs/heads/main/MLoS_Niger.csv"
 resp = requests.get(raw_url)
 if resp.status_code == 200:
     df = pd.read_csv(StringIO(resp.text))
